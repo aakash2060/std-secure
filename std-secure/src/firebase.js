3ff383522edf3945d0d3c40ef65ed9f3423af8db
Aone <aoneshrestha58@gmail.com>
2023-06-28 14:27:40 -0500

    env variables

--- std-secure/src/firebase.js
+++ std-secure/src/firebase.js
@@ -1,27 +1,31 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-import {getStorage} from "firebase/storage"
+import { getStorage } from "firebase/storage";
+import dotenv from "dotenv";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDP7r8a-u6tZFZNKhIFY7lYyF2k31GrWNo",
-  authDomain: "std-80b19.firebaseapp.com",
-  projectId: "std-80b19",
-  storageBucket: "std-80b19.appspot.com",
-  messagingSenderId: "43483549333",
-  appId: "1:43483549333:web:ae8ccbe621e1be4420d59c",
-  measurementId: "G-XEQ2SGBEZV",
+  //IN your env file your variables names should specifically be in this format (REACT_APP_*="1234")
+  apiKey: process.env.REACT_APP_API_KEY, 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();//for google signin
+export const provider = new GoogleAuthProvider(); //for google signin
 export const storage = getStorage(app);
-
